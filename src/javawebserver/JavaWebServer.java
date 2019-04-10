5f2c615e980cd025572b1cd9c5662f5c17a113d7
beltagy97 <43505298+beltagy97@users.noreply.github.com>
2019-04-10 16:41:57 +0200

    added multithreads

--- src/javawebserver/JavaWebServer.java
+++ src/javawebserver/JavaWebServer.java
@@ -7,19 +7,28 @@ package javawebserver;
 
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class JavaWebServer {
+public class JavaWebServer implements Runnable{
 
     private static final int port = 4000;
     private static final String IndexFile = "Index.html";
     private static final String FileNotFound = "404.html";
 
+    private Socket client;
+
+    public JavaWebServer(Socket skt) {
+        client = skt;
+    }
+
 //    response message hander
-    public void SendResponse(BufferedReader input,PrintWriter out ,BufferedOutputStream dataOut ,boolean isValidMethod) {
+    public void SendResponse(BufferedReader input, PrintWriter out, BufferedOutputStream dataOut, boolean isValidMethod) {
 
     }
 
@@ -31,17 +40,34 @@ public class JavaWebServer {
             System.out.println("Web Server has started successfully");
 
             while (true) {
-                
-                    Socket socket = server.accept();
-                    System.out.println("connection established");
-                
 
+                JavaWebServer socket = new JavaWebServer(server.accept());
+                Thread clientRequest = new Thread(socket);
+                clientRequest.start();
             }
 
         } catch (IOException e) {
             System.out.println("Server connection error : " + e);
         }
 
+    }
+
+    /**
+     *
+     * @throws IOException
+     */
+    @Override
+    public void run() {
+        
+        System.out.println("Just connected to " + client.getRemoteSocketAddress());
+        DataInputStream in;
+        try {
+            in = new DataInputStream(client.getInputStream());
+            System.out.println(in.readUTF());
+        } catch (IOException ex) {
+            Logger.getLogger(JavaWebServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
 
 }
